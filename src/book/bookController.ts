3f3539bdb05d35daf16f6724c1ee2f6084e22f20
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-01 17:09:49 +0530

    add author id to new book

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -4,6 +4,7 @@ import path from "node:path";
 import createHttpError from "http-errors";
 import bookModel from "./bookModel";
 import fs from "node:fs";
+import { AuthRequest } from "../middlewares/authenticate";
 
 const createBook = async (req: Request, res: Response, next: NextFunction) => {
   const { title, genre } = req.body;
@@ -42,10 +43,12 @@ const createBook = async (req: Request, res: Response, next: NextFunction) => {
       }
     );
 
+    const _req = req as AuthRequest;
+
     const newBook = await bookModel.create({
       title,
       genre,
-      author: "667a95c5bf5677f1091e1fb1",
+      author: _req.userId,
       coverImage: uploadResult.secure_url,
       file: bookFileUploadResult.secure_url,
     });
@@ -82,7 +85,10 @@ async function cleanupTempFiles(filePaths: string[]) {
         try {
           await deleteFile(filePath);
         } catch (cleanupError) {
-          console.error(`Error while cleaning up file ${filePath}:`, cleanupError);
+          console.error(
+            `Error while cleaning up file ${filePath}:`,
+            cleanupError
+          );
         }
       }
     })
