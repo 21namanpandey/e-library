8eba3e19f52df6351b937cf181f7b9b0e4315ea8
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-01 12:48:31 +0530

    add book model

---
+++ src/book/bookRouter.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { createBook } from "./bookController";
+
+const bookRouter = express.Router();
+
+// Routes
+//  /api/books
+bookRouter.post("/", createBook);
+
+export default bookRouter;
