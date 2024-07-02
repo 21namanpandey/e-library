e11e7b3e1950b6332d82059dbaa38801ebfd553f
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-02 06:35:48 +0530

    add list book endpoint

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -180,4 +180,15 @@ const updateBook = async (req: Request, res: Response, next: NextFunction) => {
   }
 };
 
-export { createBook, updateBook };
+const listBook = async (req: Request, res: Response, next: NextFunction) =>{
+  try {
+    // TODO: add pagination
+    const book = await bookModel.find()
+    
+    res.json({book})
+  } catch (error) {
+    return next(createHttpError(500, "Error while getting a book"))
+  }
+}
+
+export { createBook, updateBook , listBook};
