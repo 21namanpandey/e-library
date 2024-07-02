7483860baf552b5444f3baf9942d0ac4d9fd3738
Naman Pandey <2492004namanpandey@gmail.com>
2024-07-02 07:53:01 +0530

    add cors support

--- src/config/config.ts
+++ src/config/config.ts
@@ -10,6 +10,7 @@ const _config = {
   clodinaryCloud: process.env.CLOUDINARY_CLOUD,
   clodinaryApiKey: process.env.CLOUDINARY_API_KEY,
   clodinarySecret: process.env.CLOUDINARY_API_SECRET,
+  frontedDomain: process.env.FRONTEND_DOMAIN,
 };
 
 export const config = Object.freeze(_config);
